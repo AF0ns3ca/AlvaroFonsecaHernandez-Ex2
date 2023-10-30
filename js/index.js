7cedf7fb1f835609de22032e688c25a165466244
AF0ns3ca <afonsecahdez@gmail.com>
2023-10-30 10:30:59 +0100

    funciones añadidas, depositar, retirar, cambiarV1

--- js/index.js
+++ js/index.js
@@ -5,9 +5,33 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const screen = document.getElementById("display");
-  const PASS = "1234";
+  const btnDeposito = document.getElementById("btn-deposito");
+  const btnRetiro = document.getElementById("btn-retiro");
+  const btnTransferir = document.getElementById("btn-transferir");
+  const btnCambiar = document.getElementById("btn-cambiar");
+  const btnSalir = document.getElementById("btn-salir");
+
+  let PASS = "1234";
   let saldo = 1000;
   let intentosRestantes = 3;
+
+  const pin = () => {
+    screen.innerHTML = `
+    <span>Introduzca pin</span>
+    <input type="text" id="pin">
+`;
+  };
+
+  const mostrarSaldo = () => {
+    screen.innerHTML = `
+    <span>Bienvenido</span>
+    <span class="data">$${saldo.toFixed(2)}</span>`;
+  };
+  const actualizarSaldo = () => {
+    screen.innerHTML = `
+    <span>Saldo Actual</span>
+    <span class="data">$${saldo.toFixed(2)}</span>`;
+  };
 
   const login = () => {
     let pin = prompt(
@@ -26,15 +50,96 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const mostrarSaldo = () => {
+  const depositarDinero = () => {
     screen.innerHTML = `
-    <span>Bienvenido</span>
-    <span class="data">$${saldo.toFixed(2)}</span>`;
+        <span>Introduzca la cantidad a depositar</span>
+        <input type="number" id="saldoDep">
+        <button id="btn-save-deposito">Depositar</button>
+    `;
+    document
+      .getElementById("btn-save-deposito")
+      .addEventListener("click", () => {
+        let deposito = parseFloat(document.getElementById("saldoDep").value);
+        if (!isNaN(deposito) && deposito > 0) {
+          console.log(deposito);
+          saldo += deposito;
+          console.log(saldo);
+          actualizarSaldo();
+        } else {
+          //En este caso de momento no se implementa while que pida todo el rato, si da este mensaje hay que apretar de nuevo el boton
+          screen.innerHTML = `Error en la operación.`;
+        }
+      });
   };
-  const depositarDinero = () => {};
-  const retirarDinero = () => {};
-  const transferirDinero = () => {};
-  const cambiarPass = () => {};
+  const retirarDinero = () => {
+    screen.innerHTML = `
+        <span>Introduzca la cantidad a retirar</span>
+        <input type="number" id="saldoRet">
+        <button id="btn-save-retirada">Retirar</button>
+    `;
+    document
+      .getElementById("btn-save-retirada")
+      .addEventListener("click", () => {
+        let retiro = parseFloat(document.getElementById("saldoRet").value);
+        if (retiro < saldo && !isNaN(retiro) && retiro > 0) {
+          saldo -= retiro;
+          console.log(saldo);
+          actualizarSaldo();
+        } else {
+          //En este caso de momento no se implementa while que pida todo el rato, si da este mensaje hay que apretar de nuevo el boton
+          screen.innerHTML = `Error en la operación.`;
+        }
+      });
+  };
+  const transferirDinero = () => {
+  };
+
+  const cambiarPass = () => {
+    let intentos = 3;
+    console.log("btn working");
+    screen.innerHTML = `
+        <span>Introduzca su pin actual</span>
+        <input type="text" id="pin">
+        <button id="btn-continue">Continuar</button>
+    `;
+
+    document.getElementById("btn-continue").addEventListener("click", () => {
+      if (PASS === document.getElementById("pin").value) {
+        screen.innerHTML = `
+              <span>Introduzca nuevo pin</span>
+              <input type="text" id="nuevoPin">
+              <button id="btn-save">Guardar Cambios</button>
+          `;
+        document.getElementById("btn-save").addEventListener("click", () => {
+          console.log(PASS);
+          PASS = document.getElementById("nuevoPin").value;
+          console.log("Nuevo pin: " + PASS);
+          screen.innerHTML = `
+                  <span>Pin cambiado correctamente</span>
+              `;
+        });
+      } else {
+        screen.innerHTML = `
+                  <span>Ha ocurrido un error inesperado. Intentelo otra vez.</span>
+              `;
+      }
+    });
+  };
+
+  //Funcion que al pulsar el boton salir nos lleva a una pagina de despedida
+  const salir = () => {
+    window.location.href = "templates/salida.html";
+  };
+
+// const estructuraValidaIBAN = (cuentaValidar)  {
+  //   return /^ES\d{22}$/g.test(cuentaValidar);
+  // }
+  
 
   login();
+  btnDeposito.addEventListener("click", depositarDinero);
+  btnRetiro.addEventListener("click", retirarDinero);
+  //btnTransferir.addEventListener("click", transferirDinero);*/
+  btnCambiar.addEventListener("click", cambiarPass);
+  btnSalir.addEventListener("click", salir);
 });
