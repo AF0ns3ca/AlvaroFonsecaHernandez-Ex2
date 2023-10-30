35e1777ebe6e835bc4b0072fdee184330c4f7bea
AF0ns3ca <afonsecahdez@gmail.com>
2023-10-30 09:21:11 +0100

    templates añadido, cajerobloqueado y salida. Funcion login

--- js/index.js
+++ js/index.js
@@ -3,4 +3,38 @@
  * @GitHub https://github.com/AF0ns3ca/AlvaroFonsecaHernandez-Ex2.git
  */
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const screen = document.getElementById("display");
+  const PASS = "1234";
+  let saldo = 1000;
+  let intentosRestantes = 3;
+
+  const login = () => {
+    let pin = prompt(
+      `Introduzca pin. Intentos restantes: ${intentosRestantes}`
+    );
+    while (pin !== PASS && intentosRestantes > 1) {
+      intentosRestantes--;
+      console.log(`Pin incorrecto. Intentos restantes: ${intentosRestantes}`);
+      pin = prompt(`Pin incorrecto. Intentos restantes: ${intentosRestantes}`);
+    }
+    if (pin == PASS) {
+      console.log("Bienvenido");
+      mostrarSaldo();
+    } else {
+      window.location.href = "templates/cajeroBloqueado.html";
+    }
+  };
+
+  const mostrarSaldo = () => {
+    screen.innerHTML = `
+    <span>Bienvenido</span>
+    <span class="data">$${saldo.toFixed(2)}</span>`;
+  };
+  const depositarDinero = () => {};
+  const retirarDinero = () => {};
+  const transferirDinero = () => {};
+  const cambiarPass = () => {};
+
+  login();
+});
